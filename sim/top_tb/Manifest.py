8226652bc4493dbc4ec9d5b3a6b4f6b300386ec7
Jonas Tørnes <jonas.tornes@gmail.com>
2022-02-11 15:28:14 +0100

    Change name for tb

--- sim/top_tb/Manifest.py
+++ sim/top_tb/Manifest.py
@@ -1,9 +1,9 @@
 action = "simulation"
 sim_tool = "modelsim"
-sim_top = "top_tb"
+sim_top = "add_sub_tb"
 
-sim_post_cmd = "vsim -do ../vsim.do -c top_tb"
+sim_post_cmd = "vsim -do ../vsim.do -c add_sub_tb"
 
 modules = {
-  "local" : [ "../../test/top_tb" ],
+  "local" : [ "../../test/add_sub_tb" ],
 }
